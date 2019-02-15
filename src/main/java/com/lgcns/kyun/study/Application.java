26e54ece27e5c98df4d9a3c5f4fb26cd26bbc862
김연균 <shro1004@gmail.com>
2019-02-15 15:45:28 +0900

    5step 수정

--- src/main/java/com/lgcns/kyun/study/Application.java
+++ src/main/java/com/lgcns/kyun/study/Application.java
@@ -1,10 +1,13 @@
 package com.lgcns.kyun.study;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
+@EnableAutoConfiguration(exclude= {DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class Application {
 
