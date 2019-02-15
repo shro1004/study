80ba1a6696a023f31a8009254eb1fc166c74808c
김연균 <shro1004@gmail.com>
2019-02-15 15:52:12 +0900

    원복

--- src/main/java/com/lgcns/kyun/study/Application.java
+++ src/main/java/com/lgcns/kyun/study/Application.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@EnableAutoConfiguration(exclude= {DataSourceAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude= {DataSourceAutoConfiguration.class})
 public class Application {
 
 	public static void main(String[] args) {
